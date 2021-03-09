b50daa2136a25e719a93290ade93f5e2830adcf1
Manish Kumar <manishmania20@gmail.com>
2021-03-09 16:22:46 +0530

    changed limits

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('https://my-json-server.typicode.com/manishmania20/dummy-data/todos?_limit=7')
+    axios.get('https://my-json-server.typicode.com/manishmania20/dummy-data/todos?_limit=9')
       .then(res => this.setState({ todos: res.data }))
   }
 
